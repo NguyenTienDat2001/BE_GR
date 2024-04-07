b845eb6b0f82cadf048fe9c8c2dd5c05e009fa2c
tiendat080201 <tiendat.nguyen@secomus.com>
2024-04-07 18:18:13 +0700

    u1

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("mybook", "root", null, {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_PASSWORD || '', null, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   logging: false,
 });
